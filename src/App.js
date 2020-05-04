17e4d4055321112c5b54b49d999c5c946f9f5732
Victor Ingman <viregistermail@gmail.com>
2020-05-04 16:19:55 +0200

    PoC works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 
 const AppWrapper = styled.div`
@@ -21,19 +21,85 @@ const Flex = styled.div`
 const Title = styled.p`
   font-weight: bold;
   text-align: center;
+  margin: 0.5rem;
+`
+
+const SubTitle = styled.p`
+  color: darkgray;
+  margin-top: 0.5rem;
+  text-align: center;
 `
 
 function App() {
+  const [input, setInput] = useState()
+  const [output, setOutput] = useState()
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+  const parseJSON = (str) => {
+    try {
+      return JSON.parse(str);
+    } catch(e) {
+      console.log(e)
+      // set error or something
+    }
+  }
+  const handleDownload = (e) => {
+    e.preventDefault();
+    const { URL, Blob } = window
+    const data = new Blob([output])
+    const a = document.createElement('a')
+    a.href = URL.createObjectURL(data);
+    a.style.display = 'none'
+    a.download=`trello-csv-${new Date().toISOString()}.csv`
+    document.body.appendChild(a)
+    a.click()
+    a.remove()
+  }
+  useEffect(() => {
+    if (input == null || input == false) return
+    const timerId = setTimeout(() => {
+      const json = parseJSON(input)
+      const lists = json.lists.reduce((acc, list) => ({
+        ...acc,
+        [list.id]: list
+      }), {})
+      const allChecklists = json.checklists.reduce((acc, checkList) => ({
+        ...acc,
+        [checkList.id]: checkList
+      }), {})
+      const csvHeaders = ['name', 'description', 'listName', 'createdBy', 'checklists']
+      const csvContent = json.cards.map((card) => {
+        const { description, name, idList, idChecklists } = card
+        const { name: listName } = lists[idList] || {}
+        const checklists = idChecklists.map(
+          (checklistId) => allChecklists[checklistId]
+            .checkItems
+            .map((item) => `${item.name}:${item.state}`)
+            .join('-')
+        ).join('--')
+        return [name, description, listName, null, checklists ]
+      })
+      const csv = [
+        csvHeaders.join(','),
+        ...csvContent
+      ]
+      setOutput(csv.join('\n'));
+    }, 500);
+    return () => clearTimeout(timerId)
+  }, [input, setOutput])
   return (
     <AppWrapper>
       <Flex>
         <Flex n="1" col>
           <Title>JSON</Title>
-          <TextArea>Hello</TextArea>
+          <SubTitle>Input JSON from Trello export</SubTitle>
+          <TextArea value={input} onChange={handleInput}></TextArea>
         </Flex>
         <Flex n="1" col>
           <Title>CSV</Title>
-          <TextArea>Hello2</TextArea>
+          <SubTitle>Output in CSV format <a onClick={handleDownload} href="#download">[Download]</a></SubTitle>
+          <TextArea value={output} disabled></TextArea>
         </Flex>
       </Flex>
     </AppWrapper>
